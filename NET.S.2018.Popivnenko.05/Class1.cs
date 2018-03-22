5e5eee00d07a60cae23b24d174c5f4dac0e5f8b2
crocomoth <justletmeplaymf@gmail.com>
2018-03-22 12:36:15 +0300

    Add project files.

---
+++ NET.S.2018.Popivnenko.05/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2018.Popivnenko._05
+{
+    public class Class1
+    {
+    }
+}
